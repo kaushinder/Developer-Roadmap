593bd3127daed9dc9cabe163ec9a7fcf325d1edd
kaushinder <kaushindersinghraghav@gmail.com>
2026-04-07 10:27:14 +0530

    Episode-02 |   Igniting our App

--- LearningProjects/REACT/app.js
+++ LearningProjects/REACT/app.js
@@ -1,14 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
-  { className: "parent" },
+  { className: "parent", key: "parent" },
   [
-    React.createElement("div", { className: "child1" }, [
-      React.createElement("h1", { className: "heading1" }, "I am child 1"),
-      React.createElement("h2", { className: "heading2" }, "I am child 1.1"),
+    React.createElement("div", { className: "child1", key: "child1" }, [
+      React.createElement("h1", { className: "heading1", key: "heading1" }, "I am child 1"),
+      React.createElement("h2", { className: "heading2", key: "heading2" }, "I am child 1.1"),
     ]),
-    React.createElement("div", { className: "child2" }, [
-      React.createElement("h3", { className: "heading3" }, "I am child 2"),
-      React.createElement("h4", { className: "heading4" }, "I am child 2.1"),
+    React.createElement("div", { className: "child2", key: "child2" }, [
+      React.createElement("h3", { className: "heading3", key: "heading3" }, "I am child 2"),
+      React.createElement("h4", { className: "heading4", key: "heading4" }, "I am child 2.1"),
     ]),
   ],
   "Hello, React!",
