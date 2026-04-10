bb84313aecb0feeaa11de2296009f04345556cf3
kaushinder <kaushindersinghraghav@gmail.com>
2026-04-10 22:34:09 +0530

    Episode-04 |   Talk is Cheap, Show me the Code

--- LearningProjects/REACT/app.js
+++ LearningProjects/REACT/app.js
@@ -41,7 +41,7 @@ const RestaurantCard = ({ resData }) => {
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img src={IMG_CDN + cloudinaryImageId} alt={name} className="res-logo" />
       <h3>{name}</h3>
-      <h4>{avgRating} ⭐</h4>
+      <h4>{avgRating} stars ⭐</h4>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{deliveryTime}</h4>
       <h4>{costForTwo}</h4>
